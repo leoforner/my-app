beb4afe6bbdd0c8fabafb19a1356463eeefbe546
leonardo <leonardoaguiarforner@gmail.com>
2020-08-18 16:43:32 -0300

    commit1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Menu from './components/Menu'; 
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer'
+import React from '../../../node_modules/react';
+import Menu from '../../components/Menu'; 
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer'
 
-function App() {
+function Home() {
   return (
     <div style={{background: "#141414" }} >
       <Menu />
@@ -33,8 +33,17 @@ function App() {
 				category={dadosIniciais.categorias[3]}
 			/>
 
+			<Carousel
+				category={dadosIniciais.categorias[4]}
+			/>
+			
+			<Carousel
+				category={dadosIniciais.categorias[5]}
+			/>
+
+			<Footer />
     </div>
 	);
 }
 
-export default App;
+export default Home;
